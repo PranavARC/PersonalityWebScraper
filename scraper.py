d44e4d77cd33aa30d10a6bda8dd6d733959b6b54
Pranav Reddy Charkupalli <pranavcharku07@gmail.com>
2020-08-27 17:41:38 -0500

    Reading Functionality Added
    Lets the user choose to open a webpage to read the article

--- scraper.py
+++ scraper.py
@@ -1,5 +1,7 @@
 from urllib.request import urlopen
 from html import unescape
+from webbrowser import open
+from sys import exit
 OLDEST = 83 # oldest page number as of August 27th, 2020
 
 # Webpage scraping code
@@ -8,7 +10,20 @@ def webscrape(url):
     html_bytes = page.read()
     return html_bytes.decode("utf-8")
 
-    
+# Function to choose between a range of pages/articles
+# is_art = "article index you want to check out" or "Smashboards page you want to browse"
+def chooser(is_art, high):
+    while(True):
+        select = input("Please enter the " + is_art + " (1-" + str(high) + "): ")
+        try:
+            select = int(select)
+        except ValueError:
+            input("Input not numeric, press enter to try again ")
+        else:
+            if(1 <= select <= high):
+                return select
+            else:
+                input("Input not between 1 and " + str(high) + ", press enter to try again ")
 
 # Finding the oldest page number
 scrape = webscrape("https://smashboards.com/news")
@@ -21,31 +36,30 @@ except ValueError:
     old_page = OLDEST   # in case oldest page scrape doesn't work
 
 # Take a given page number from 1 to old_page and return the first article of that page
-while(True):
-    page_no = input("Please enter the Smashboards page you want to extract from (1-" + str(old_page) + "): ")
-    try:
-        page_no = int(page_no)
-    except ValueError:
-        input("Input not numeric, press enter to try again ")
-    else:
-        if(1 <= page_no <= old_page):
-            break
-        else:
-            input("Input not between 1 and " + str(old_page) + ", press enter to try again ")
+page_no = chooser("Smashboards page you want to browse", old_page)
 
-# The first article on Smashboards starts with a <span>
-# so extract words between the first <span> and its corresponding </span>
+# Check the number of articles on that page (usually 20 for every page but the last)
 scrape = webscrape("https://smashboards.com/news/page-"+ str(page_no))
 max_art = scrape.count("<span>")
-print(max_art)
-start_index = scrape.find("<span>") + len("<span>")
+art_no = chooser("article index you want to check out", max_art)
+
+# Way to dynamically choose the (n-1)th index. WARNING: Corrupts the original scrape
+start_index = scrape.replace('<span>', '777777', art_no-1).find('<span>') + len("<span>")
 end_index = scrape.find("</span>", start_index)
-first_title = scrape[start_index:end_index]
+art_title = scrape[start_index:end_index]
 
 # Removes tabs, newlines, spaces, and HTML character codes in the extracted string
-str_list = first_title.split()
-first_title = " ".join(str_list)
-print(unescape(first_title))
+str_list = art_title.split()
+art_title = " ".join(str_list)
+print("The article's title is: " + unescape(art_title))
+
+# Open a webbrowser to the scraped article link if chosen
+choice = input("Do you want to read it? (Y/N): ")
+if(choice != 'Y' and choice != 'y'):
+    exit(0)
+start_index = scrape.rfind("<a href=\"", 0, start_index) + len("<a href=\"")
+end_index = scrape.find("\">", start_index)
+open("https://smashboards.com" + scrape[start_index:end_index], new=0, autoraise=True)
 
 # To print into a file for checking
 # a_file = open("output.txt", "w")
