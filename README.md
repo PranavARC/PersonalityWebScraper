b7748e50929341d218b415f59ecdebc1c6952b82
Pranav Reddy Charkupalli <pranavcharku07@gmail.com>
2020-09-01 17:45:41 -0500

    "Days since" functionality
    viewer.py now prints days since the last article

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # SmashboardsArticleScraper
- Personal project to display the article titles on a given news page of https://smashboards.com and to select one of them for reading in a web browser.
+ Personal project to display the article titles on a given news page of https://smashboards.com and to select one of them for reading in a web browser (scraper.py).
 
  An experiment with web scraping technologies in Python.
 
- ## Branch Functionality ##
+ ## Branch Descriptions ##
 master: Lists out every article  
-lister: Doesn't list articles  
-looper: Allows selection of different page
+timer: Corrects for timezones in viewer.py (requires user to install pytz on their machine)
 
-master also contains an extra file (viewer.py) that lets me see the views on each of my articles
+master and timer contain an extra file (viewer.py) that lets me see the views on each of my articles, the dates they were written, my total views, and the days since I wrote my last article.
+
+(outdated)  
+lister: Doesn't list articles  
+looper: Allows selection of different page without relaunching
--- viewer.py
+++ viewer.py
@@ -1,6 +1,7 @@
 from urllib.request import urlopen
 from html import unescape
 from operator import attrgetter
+import datetime
 
 # Webpage scraping code
 def webscrape(url):
@@ -15,7 +16,7 @@ class article:
         self.views = views
         self.date = date
     def __str__(self):
-        return self.title + ": " + str(self.views) + " (" + self.date + ")"
+        return self.title + ": " + str(self.views) + " (" + self.date.strftime("%b %d, %Y") + ")"
 
 # Check the number of articles on that page, and list their titles and view counts by looping
 scrape = webscrape("https://smashboards.com/news/authors/arcain.421960/")
@@ -23,6 +24,7 @@ max_art = scrape.count("<span>")
 begin = 0
 total_views = 0
 articles = []
+last_date = 0
 
 for i in range(1, max_art + 1):
     # Find the title first
@@ -35,9 +37,11 @@ for i in range(1, max_art + 1):
     begin = end_index
 
     # Find the publication date next
-    start_index = scrape.find("data-date-string=\"", start_index) + len("data-date-string=\"")
-    end_index = scrape.find("\"", start_index)
-    pub_date = scrape[start_index:end_index]
+    start_index = scrape.find("datetime=\"", start_index) + len("datetime=\"")
+    end_index = scrape.find("\"", start_index) - 5
+    date_obj = datetime.datetime.strptime((scrape[start_index:end_index]).replace("T", " "), "%Y-%m-%d %H:%M:%S")
+    if(i == 1):
+        last_date = date_obj    # Date of the latest article
 
     # Find the view count last
     start_index = scrape.find("<i class=\"fa--xf far fa-eye\" aria-hidden=\"true\"></i>", begin) + len("<i class=\"fa--xf far fa-eye\" aria-hidden=\"true\"></i>")
@@ -47,8 +51,8 @@ for i in range(1, max_art + 1):
     str_list = view_count.split()
     view_count = int((" ".join(str_list)).replace(",", ""))
     total_views += view_count
-    # begin = end_index # in case
-    articles.append(article(art_title, view_count, pub_date))    
+    # begin = end_index # Just in case
+    articles.append(article(art_title, view_count, date_obj))    
 
 # Sorting the array by descending view counts and printing
 articles.sort(key=attrgetter('views'), reverse=True)
@@ -57,4 +61,12 @@ for i in articles:
     print(str(num) + ". " + str(i))
     num += 1
 
-input("Total views: " + str(total_views))
+# Find days since last article
+days_since = ((datetime.datetime.now()) - last_date).days
+# To prevent negative days from timezone issues
+if(days_since < 0):
+    days_since = 0
+
+print("\nTotal views: " + str(total_views))
+print("Days since last article: " + str(days_since))
+input("")
