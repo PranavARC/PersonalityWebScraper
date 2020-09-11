c040b691a0ac2478d2c5ba0feb4260c1f8b60e7c
Pranav Reddy Charkupalli <pranavcharku07@gmail.com>
2020-09-10 22:24:35 -0500

    Changed viewer.py to accept every author
    viewer.py will now be able to list the same details as before for every author on the site, as well as listing them all out.

--- README.md
+++ README.md
@@ -1,14 +1,19 @@
 # SmashboardsArticleScraper
- Personal project to display the article titles on a given news page of https://smashboards.com and to select one of them for reading in a web browser (scraper.py).
+ Personal project to either:  
+ 1. display the article titles on a given news page of https://smashboards.com and select one of them for reading in a web browser (scraper.py) or  
+ 2. view the articles of a particular author, listed from most to least viewed, and their total view count (viewer.py)
 
- An experiment with web scraping technologies in Python.
+ An experiment with web scraping technologies in Python using the urllib package.
 
- ## Branch Descriptions ##
-master: Lists out every article  
+ ## Branch Descriptions ##  
 timer: Corrects for timezones in viewer.py (requires user to install pytz on their machine)
 
-master and timer contain an extra file (viewer.py) that lets me see the views on each of my articles, the dates they were written, my total views, and the days since I wrote my last article.
-
-(outdated)  
+(outdated, only have scraper.py)  
 lister: Doesn't list articles  
 looper: Allows selection of different page without relaunching
+
+## Installation ##
+Only requires installation of pytz to system for timer branch and best performance  
+```shell
+$ pip install pytz
+```
--- viewer.py
+++ viewer.py
@@ -1,6 +1,8 @@
 from urllib.request import urlopen
 from html import unescape
 from operator import attrgetter
+from difflib import SequenceMatcher
+from time import sleep
 import datetime
 try:
     import pytz
@@ -13,7 +15,7 @@ def webscrape(url):
     html_bytes = page.read()
     return html_bytes.decode("utf-8")
 
-# An article class to put objects in an array
+# Article and author classes to create arrays of them
 class article:
     def __init__(self, title, views, date, age):
         self.title = title
@@ -23,8 +25,107 @@ class article:
     def __str__(self):
         return self.title + ": " + str(self.views) + " (" + self.date.strftime("%b %d, %Y") + ")"
 
+class author:
+    def __init__(self, num, name, tag, link):
+        self.num = num
+        self.name = name
+        self.tag = tag
+        self.link = link
+    def __str__(self):
+        return str(self.num) + ": " + self.name
+
+# Searching the author's page for every Smashboards writer
+magic_str = """<div class=\"porta-author-name\">
+								<a href=\"/news/authors/"""
+num_str = ""
+scrape = webscrape("https://smashboards.com/news/authors")
+max_auth = scrape.count(magic_str)
+authors = []
+begin = 0
+
+# Putting every author(their number, name, gamertag if given, and link) in an array
+# start_index to mid_index is their link, mid_index to end_index is their name
+for i in range(1, max_auth + 1):
+    start_index = scrape.find(magic_str , begin) + len(magic_str)
+    mid_index = scrape.find("/\">", start_index)
+    link = scrape[start_index:mid_index]
+    mid_index += 3  # \"> is 3 characters
+    end_index = scrape.find("</a>", mid_index)
+    name = scrape[mid_index:end_index]
+
+    # Remove whitespace, HTML character codes in art_title with split, join, unescape
+    str_list = name.split()
+    name = unescape(" ".join(str_list))
+    
+    # If the name has 2 quotes, the author has a gamertag, else leave it blank
+    tag = ""
+    if(name.count("\"") == 2):
+        t_start = name.find("\"") + 1
+        t_end = name.find("\"", t_start)
+        tag = name[t_start:t_end]
+    authors.append(author(i, name, tag, link))
+    begin = end_index
+
+# An infinite loop until the user can provide the author they want to check out
+while(True):
+    choice = input("Enter the " + num_str + "name/tag of the author you wish to check out, or enter \"list\" to see a list of them: ")
+    if(choice.isnumeric()):
+        if(num_str == ""):  # This should only work once the authors have been listed
+            print("Invalid entry, try again")
+            sleep(0.5)
+        
+        else:   # If the number is between 1 and 73, author has been found
+            if(1 <= int(choice) <= max_auth):
+                num_str = authors[int(choice) - 1].link
+                break
+            else:
+                print("Invalid number, try again")                
+                sleep(0.5)
+
+    elif(choice.lower() == "list"): # List out every author
+        for i in authors:
+            print(i)
+        num_str = "number or "  # Change this to show that authors have been listed
+    
+    else:   # A name was provided
+        if(choice == ""):   # An empty name might match with an empty gamertag, so reject it
+            print("Invalid entry, try again")
+            sleep(0.5)
+            continue
+
+        for i in authors:   # Check the entire authors array for a match
+            s1 = choice.lower()
+            s2 = i.name.lower()
+            s3 = i.tag.lower()
+            match = ""
+            if(s1 == s2 or s1 == s3):   # If an exact match occurs with name or gamertag, author has been found 
+                num_str = i.link
+                break
+
+            # SequenceMatcher lets you check if the names typed in were close to correct
+            elif(SequenceMatcher(a=s1,b=s2).ratio() >= 0.7):    # name
+                match = i.name
+            elif(SequenceMatcher(a=s1,b=s3).ratio() >= 0.7):    # tag
+                match = i.tag
+            else:   # Neither the tag or name were close matches, so move on to the rest of the array
+                continue
+
+            choice = input("Did you mean " + match + " (Y/N)? ")
+            if(choice.isalpha() and (str(choice)).upper() == 'Y'):  # If Y, author has been found
+                num_str = i.link
+                break
+
+        # if num_str is still "", no author has been found, so redo the loop
+        if(num_str == ""):
+            print("Invalid entry, try again")
+            sleep(0.5)
+        else:
+            break
+
+print("")
+
 # Check the number of articles on that page, and list their titles and view counts by looping
-scrape = webscrape("https://smashboards.com/news/authors/arcain.421960/")
+scrape = webscrape("https://smashboards.com/news/authors/" + num_str)
 max_art = scrape.count("<span>")
 begin = 0
 total_views = 0
@@ -68,6 +169,12 @@ for i in articles:
         newest = str(num)
     num += 1
 
+# astimezone causes an error if last_date is still 0
+if(last_date == 0):
+    print("They haven't written any articles yet")
+    sleep(2)
+    exit(0)
+
 # Use of pytz library to sync timezones (SmashBoards runs on PST)
 try:
     pst = pytz.timezone('Europe/Berlin')
@@ -85,4 +192,4 @@ if(days_since < 0):
 
 print("\nTotal views: " + str(total_views))
 print("Days since last article: " + str(days_since) + " (" + newest + ")")
-input("")
+sleep(5)
